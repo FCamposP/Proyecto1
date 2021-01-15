a4b35eeee2ef718883f705cca6fd617c079f4bcb
AndreaMartinez77 <mg14082@ues.edu.sv>
2021-01-14 19:36:41 -0600

    Se pasó el html a Angular

--- src/app/live-chat/live-chat.component.ts
+++ src/app/live-chat/live-chat.component.ts
@@ -5,9 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './live-chat.component.html',
   styleUrls: ['./live-chat.component.css']
 })
-export class LiveChatComponent implements OnInit {
 
-  constructor() { }
+export class LiveChatComponent implements OnInit {
+  usuarioActual = 'usuario actual';
+  usuarioAlmacenado = 'usuario almacenado';
+  usuarioLocal = 'Usuario Local';
+  mensajeUsuarioActual = 'Mensaje de usuario actual';
+  mensajeUsuarioAlmacenado = 'Mensaje de usuario almacenado';
+  mensajeUsuarioLocal = 'Mensaje de usuario local';
+  fotoUsuarioActual = 'assets/user.png';
+  fotoAlmacenado = 'assets/user.png';
+  fotoUsuarioAlmacenado = 'assets/user.png';
+  horaUsuarioActual = '00:00';
+  horaUsuarioAlmacenado = '00:02';
+  horaUsuarioLocal = '00:03';
+
+
+  constructor() {
+
+    }
 
   ngOnInit() {
   }
